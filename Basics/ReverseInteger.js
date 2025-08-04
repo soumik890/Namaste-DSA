eaa32521dc374b78c9e08020f9921ac05ffdf515
soumik890 <soumik890@gmail.com>
2025-08-04 14:06:49 +0530

    new_changes

--- Basics/ReverseInteger.js
+++ Basics/ReverseInteger.js
@@ -9,3 +9,16 @@ const reverseInteger = (number) => {
 
   return output;
 };
+
+const reverseInterger2 = (n) => {
+  let rev = 0;
+  let number = Math.abs(n);
+  while (number > 0) {
+    let last = number % 10;
+    rev = rev * 10 + last;
+    number = Math.floor(number / 10);
+  }
+  return n < 0 ? -rev : rev;
+};
+
+console.log(reverseInterger2(-1234567890));
