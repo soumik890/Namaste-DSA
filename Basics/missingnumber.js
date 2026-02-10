f533acac7c1b53db986fa22ae30879cc5d6bf65c
soumik890 <soumik890@gmail.com>
2026-02-10 22:15:43 +0530

    missing number

--- Basics/missingnumber.js
+++ Basics/missingnumber.js
@@ -1,12 +1,15 @@
 const missingNumber = (nums) => {
-  const consecutiveArray = [...Array(nums.length+1).keys()];
+  let n = nums.length;
+  let totalSum = (n * (n + 1)) / 2;
+  // console.log(totalSum);
 
+  let partialSum = 0;
 
-  for (let i = 0; i < consecutiveArray.length; i++) {
-    if (!nums.includes(consecutiveArray[i])) {
-      return consecutiveArray[i];
-    }
+  for (let i = 0; i < nums.length; i++) {
+    partialSum = partialSum + nums[i];
   }
+
+  return totalSum - partialSum;
 };
 
 console.log(missingNumber([3, 0, 1]));
